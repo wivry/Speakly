03058a0caaa52a37355aaaf76e6bd808fdf00e80
Marek Vavrinek <vavrim10@fel.cvut.cz>
2023-10-18 16:18:51 +0200

    přidání komponentu GoTo a úpravy kódů 2

---
+++ Speakly/frontend/src/components/GoTo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// komponent tlačítka pro přechod na jinou stránku
+function GoTo(props) {
+  return (
+    <div className="shadow-lg custom-card mt-5 mb-5">
+      <div className="row">
+        <div className="col-3" />
+        <a
+          className={
+            props.GoToPage.blue
+              ? "col-6 btn btn-lg shadow-lg btn-primary custom-rounded"
+              : "col-6 btn btn-lg shadow-lg btn-secondary custom-rounded"
+          }
+          role="button"
+          href={props.GoToPage.href}
+        >
+          {props.GoToPage.name}
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default GoTo;
