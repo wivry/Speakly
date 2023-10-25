cf128ae79898f79e59529a74312eb69f56d2b37d
Marek Vavrinek <vavrim10@fel.cvut.cz>
2023-10-25 16:35:17 +0200

    DEBUG true

--- Speakly/frontend/src/components/App.js
+++ Speakly/frontend/src/components/App.js
@@ -4,11 +4,19 @@ import HomePage from "./HomePage";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+import { BrowserRouter } from "react-router-dom";
 
 // komponent vykreslující stránku Reactu do šablony pro Django
 
-const root = ReactDOM.createRoot(document.getElementById("main"));
+/*ReactDOM.render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+*/
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HomePage />
