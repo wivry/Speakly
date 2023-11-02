c211111b82d6c7632f4b0d01a94221bb6adfb7d2
Marek Vavrinek <vavrim10@fel.cvut.cz>
2023-11-02 09:57:02 +0100

    oprava static

--- Speakly/Speakly/settings.py
+++ Speakly/Speakly/settings.py
@@ -127,13 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/sredemo/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media settings (determines where images will be uploaded)
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/sredemo/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
